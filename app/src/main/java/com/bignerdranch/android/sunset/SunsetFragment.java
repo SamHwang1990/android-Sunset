57bc4b79992e3dee378a9612202292c4be555002
SamHwang1990 <samhwang1990@gmail.com>
2016-08-30 22:28:48 +0800

    complete the 30th chapter practices: 1. add multi animator; 2. add AnimatorSet;

--- app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -1,12 +1,17 @@
 package com.bignerdranch.android.sunset;
 
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 
 /**
  * Created by sam on 16/8/30.
@@ -16,6 +21,10 @@ public class SunsetFragment extends Fragment {
     private View mSceneView;
     private View mSkyView;
     private View mSunView;
+
+    private int mBlueSkyColor;
+    private int mSunsetSkyColor;
+    private int mNightSkyColor;
 
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
@@ -35,6 +44,18 @@ public class SunsetFragment extends Fragment {
         mSkyView = v.findViewById(R.id.sky);
         mSunView = v.findViewById(R.id.sun);
 
+        Resources resources = getResources();
+
+        if (Build.VERSION.SDK_INT > 23) {
+            mBlueSkyColor = resources.getColor(R.color.blue_sky, null);
+            mSunsetSkyColor = resources.getColor(R.color.sunset_sky, null);
+            mNightSkyColor = resources.getColor(R.color.night_sky, null);
+        } else {
+            mBlueSkyColor = resources.getColor(R.color.blue_sky);
+            mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+            mNightSkyColor = resources.getColor(R.color.night_sky);
+        }
+
         mSceneView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,7 +73,24 @@ public class SunsetFragment extends Fragment {
         ObjectAnimator heightAnimator = ObjectAnimator
                 .ofFloat(mSunView, "y", sunYStart, sunYEnd)
                 .setDuration(3000);
+        heightAnimator.setInterpolator(new AccelerateInterpolator());
 
-        heightAnimator.start();
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                .setDuration(3000);
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        ObjectAnimator nightSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mNightSkyColor)
+                .setDuration(1500);
+        nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet
+                .play(heightAnimator)
+                .with(sunsetSkyAnimator)
+                .before(nightSkyAnimator);
+
+        animatorSet.start();
     }
 }
